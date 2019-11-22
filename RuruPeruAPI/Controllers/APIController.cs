c2011dd28afa5a48d4cb8c41f4aab07af1689a03
renzo01 <renzogomez577@gmail.com>
2019-11-22 16:32:54 -0500

    Cambios para usar servicios

--- RuruPeruAPI/Controllers/APIController.cs
+++ RuruPeruAPI/Controllers/APIController.cs
@@ -6,38 +6,23 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Data.SqlClient;
 using System.Data;
-using RuruPeruAPI.Models;
+using RuruPeruAPI.Consultas;
 
 namespace RuruPeruAPI.Controllers
 {
     public class APIController : ApiController
     {
         SqlConnection cn = new SqlConnection("server=.;Database=RuruPeruDB;Integrated Security=true");
-
+        //services
+        ConsultaClient serviceCOnsulta = new ConsultaClient();
         [HttpGet]
         [Route("api/listadoUsuario")]
         public IHttpActionResult Usuario() {
-            List<Usuario> list = new List<Usuario>();
-            SqlCommand cmd = new SqlCommand("usp_listar_Usuarios", cn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cn.Open();
-            SqlDataReader dr = cmd.ExecuteReader();
-            while (dr.Read()) {
-                Usuario u = new Usuario()
-                {
-                    idUsuario = dr.GetString(0),
-                    nomUsuario = dr.GetString(1),
-                    nomDistrito = dr.GetString(2),
-                    fotoUsuario = dr.GetString(3),
-                    descripcionEstado = dr.GetString(4)
-                };
-                list.Add(u);
-            }
-            dr.Close(); cn.Close();
-            if (list.Count() == 0) {
+            
+            if (serviceCOnsulta.ListarUsuarios() == null) {
                 return NotFound();
             }
-            return Ok(list);
+            return Ok(serviceCOnsulta.ListarUsuarios());
         }
     }
 }
