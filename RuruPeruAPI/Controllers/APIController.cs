78e548f8b58b18843b6c5880e5428c3275961e8f
renzo01 <renzogomez577@gmail.com>
2019-11-28 16:12:20 -0500

    datos

--- RuruPeruAPI/Controllers/APIController.cs
+++ RuruPeruAPI/Controllers/APIController.cs
@@ -7,22 +7,75 @@ using System.Web.Http;
 using System.Data.SqlClient;
 using System.Data;
 using RuruPeruAPI.Consultas;
+using System.Configuration;
 
 namespace RuruPeruAPI.Controllers
 {
     public class APIController : ApiController
     {
-        SqlConnection cn = new SqlConnection("server=.;Database=RuruPeruDB;Integrated Security=true");
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["RuruPeruDB"].ConnectionString);
         //services
-        ConsultaClient serviceCOnsulta = new ConsultaClient();
+        ConsultaClient serviceConsulta = new ConsultaClient();
+
         [HttpGet]
         [Route("api/listadoUsuario")]
-        public IHttpActionResult Usuario() {
-            
-            if (serviceCOnsulta.ListarUsuarios() == null) {
+        public IHttpActionResult Usuario()
+        {
+
+            if (serviceConsulta.ListarUsuarios().Count() == 0)
+            {
                 return NotFound();
             }
-            return Ok(serviceCOnsulta.ListarUsuarios());
+            return Ok(serviceConsulta.ListarUsuarios());
+        }
+        [HttpGet]
+        [Route("api/listadoClientes")]
+        public IHttpActionResult Cliente()
+        {
+            if (serviceConsulta.ListarClientes().Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(serviceConsulta.ListarClientes());
+        }
+        [HttpGet]
+        [Route("api/listadoProveedor")]
+        public IHttpActionResult Proveedor()
+        {
+            if (serviceConsulta.ListarProveedor().Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(serviceConsulta.ListarProveedor());
+        }
+        [HttpGet]
+        [Route("api/listadoProductos")]
+        public IHttpActionResult Producto() {
+            if (serviceConsulta.ListarProductos().Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(serviceConsulta.ListarProductos());
+        }
+        [HttpGet]
+        [Route("api/listadoCategoriaProd")]
+        public IHttpActionResult CategoriaProd()
+        {
+            if (serviceConsulta.ListaCategoriaProd().Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(serviceConsulta.ListaCategoriaProd());
+        }
+        [HttpGet]
+        [Route("api/listadoEstadoUsuario")]
+        public IHttpActionResult EstadoUsuario()
+        {
+            if (serviceConsulta.ListarEstadoUsuario().Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(serviceConsulta.ListarEstadoUsuario());
         }
     }
 }
